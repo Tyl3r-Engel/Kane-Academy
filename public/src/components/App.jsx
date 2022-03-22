c344b8ea6c3ff554bae9f3f4f2081972668c21e9
Robertlh1 <robertlh1@DESKTOP-5JGUGOE.localdomain>
2022-03-22 14:56:16 -0700

    Various bugfixes to profiles and implemented profile editing

--- public/src/components/App.jsx
+++ public/src/components/App.jsx
@@ -16,6 +16,7 @@ export default function App() {
           <Route path="/login" element={<LoginRoot />} />
           <Route path="/signup" element={<SignupRoot />} />
           <Route path="/profile/*" element={<ProfileRoot />} />
+          <Route path="/profile" element={<ProfileRoot />} />
           <Route path="/fakedata" element={FakeData()} />
         </Routes>
       </Router>
