5520e53e0822496efd0e5fbd205c24506a654091
Tyler Engel <engelcrag@gmail.com>
2022-03-21 18:02:17 -0700

    basic video

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { generateData } = require('../db/fakeData.js')
+const { v4: uuidV4 } = require('uuid')
 
 const app = express();
 
@@ -25,5 +26,31 @@ app.get('/fakedata', (req, res) => {
   })
 });
 
+// * socket io stuff & video call endpoints
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+app.set('views', path.join(__dirname, '/videoCall/views'));
+app.set('view engine', 'ejs')
+
+app.get('/videoCall', (req, res) => {
+  res.redirect(`/videoCall/${uuidV4()}`)
+})
+
+app.get('/videoCall/:room', (req, res) => {
+  res.render('room', { roomId : req.params.room })
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+    socket.on('disconnect', () => {
+      socket.broadcast.to(roomId).emit('user-disconnected', userId);
+    })
+  })
+})
+
 const port = process.env.PORT || 3001;
-app.listen(port);
+server.listen(port);
