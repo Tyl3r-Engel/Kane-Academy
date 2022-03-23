83e84350aa73ef1f2b4058563598671da96a2ec1
Minseo Kim <minseo0321@gmail.com>
2022-03-23 14:09:17 -0700

    added catch statement for searchProfiles axios request

--- public/src/components/profile/Search.jsx
+++ public/src/components/profile/Search.jsx
@@ -8,9 +8,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import requestSkills from './axios/requestSkills';
 import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 
 export default function Search() {
   const [query, setQuery] = useState('')
